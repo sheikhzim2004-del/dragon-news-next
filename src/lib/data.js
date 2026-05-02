358414885993f979d4a3e36091f1d79aec319528
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-05-03 02:15:36 +0600

    show dynamic news details

--- src/lib/data.js
+++ src/lib/data.js
@@ -10,3 +10,11 @@ export async function getNewsByCategoriesId(category_id) {
     const data = await res.json();
     return data.data;
 }
+
+export async function getNewsDetailsById(news_id) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/${news_id}`,
+  );
+  const data = await res.json();
+  return data.data[0];
+}
