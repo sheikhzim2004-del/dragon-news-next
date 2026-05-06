2c740f193f14e5e2bca4ae7e08a1dfba0d0971b6
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-05-06 13:25:56 +0600

    design register page

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,13 +1,15 @@
 "use client"
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const LoginPage = () => {
+    const [showPassword, setShowPassword] =useState(false);
     const { register, handleSubmit, formState: { errors } } = useForm();
 
     const handleLoginFunc = (data) => {
-        // console.log(data, "data")
+        console.log(data, "data")
     }
     // console.log(errors, "error");
 
@@ -33,10 +35,20 @@ const LoginPage = () => {
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend">Password</legend>
                             <input
-                                type="password"
-                                {...register("password", { required: 'Password is Requered!' })}
+                                type={showPassword ? "text" : "password"}
+                                {...register("password", {
+                                    required: 'Password is Requered!',
+                                    minLength: { value: 6, message: "Password must be at least 6 characters" },
+                                    maxLength: { value: 10, message: "Password must be less than 20 characters" },
+                                    pattern: { value: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, message: "Password must contain at least one uppercase letter, one number, and one special character" },
+                                })}
                                 className="input border-0 w-full bg-slate-100 rounded-xl"
                                 placeholder="Type here Yout Password" />
+                                <button
+                                type='button'
+                                className=''
+                                onClick={() => setShowPassword(!showPassword)}
+                                >{showPassword ? <FaEyeSlash /> : <FaEye />}</button>
                         </fieldset>
                         {errors.password && <p className='text-red-600'><small>{errors.password.message}</small></p>}
                         <button className='btn w-full bg-slate-800 text-white rounded-xl mb-4'>Login</button>
--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -1,9 +1,79 @@
-import React from 'react';
+"use client"
+import Link from 'next/link';
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { FaEye, FaEyeSlash } from 'react-icons/fa';
 
 const RegisterPage = () => {
+    const [showPassword, setShowPassword] =useState(false);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    const handleRegisterFunc = (data) => {
+        console.log(data, "data")
+    }
+    // console.log(errors, "error");
+
+
+
     return (
-        <div>
-            <h1>Register page</h1>
+        <div className='min-h-[80hv] flex flex-col justify-center items-center mt-6 bg-slate-100 container mx-auto rounded-xl p-8'>
+            <div className='rounded-xl bg-white p-4'>
+                <h1 className='text-4xl font-bold text-center'>Register Your Account</h1>
+                <div>
+                    <form className='space-y-4' onSubmit={handleSubmit(handleRegisterFunc)}>
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Your Name</legend>
+                            <input
+                                type="text"
+                                {...register("name", { required: "The Name is requered" })}
+                                className="input border-0 w-full bg-slate-100 rounded-xl"
+                                placeholder="Your Name" />
+                        </fieldset>
+                        {errors.name && <p className='text-red-600'><small>{errors.name.message}</small></p>}
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Photo URL</legend>
+                            <input
+                                type="text"
+                                {...register("photo", { required: "The Photo URL is requered" })}
+                                className="input border-0 w-full bg-slate-100 rounded-xl"
+                                placeholder="Photo URL" />
+                        </fieldset>
+                        {errors.photo && <p className='text-red-600'><small>{errors.photo.message}</small></p>}
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Email Address</legend>
+                            <input
+                                type="email"
+                                {...register("email", { required: "The Email is requered" })}
+                                className="input border-0 w-full bg-slate-100 rounded-xl"
+                                placeholder="Type here Yout Email" />
+                        </fieldset>
+                        {errors.email && <p className='text-red-600'><small>{errors.email.message}</small></p>}
+
+
+                        <fieldset className="fieldset">
+                            <legend className="fieldset-legend">Password</legend>
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                {...register("password", {
+                                    required: 'Password is Requered!',
+                                    minLength: { value: 6, message: "Password must be at least 6 characters" },
+                                    maxLength: { value: 10, message: "Password must be less than 20 characters" },
+                                    pattern: { value: /^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,}$/, message: "Password must contain at least one uppercase letter, one number, and one special character" },
+                                })}
+                                className="input border-0 w-full bg-slate-100 rounded-xl"
+                                placeholder="Type here Yout Password" />
+                                <button
+                                type='button'
+                                className=''
+                                onClick={() => setShowPassword(!showPassword)}
+                                >{showPassword ? <FaEyeSlash /> : <FaEye />}</button>
+                        </fieldset>
+                        {errors.password && <p className='text-red-600'><small>{errors.password.message}</small></p>}
+                        <button className='btn w-full bg-slate-800 text-white rounded-xl mb-4'>Login</button>
+                    </form>
+                    <p>Dont have an account? <Link className='text-blue-500 text-md font-medium' href={"/login"}>Login</Link></p>
+                </div>
+            </div>
         </div>
     );
 };
